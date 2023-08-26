820e81eea62250167d648ac7167be92653a9cbb3
bristolkon <bristolkon3802@gmail.com>
2023-08-26 15:35:01 +0900

    data cache에 수정 해보기

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -52,14 +52,33 @@ const Image = styled.div`
   background-position: center center;
   border-radius: 7px;
 `;
+
 export default function Movie() {
   const { id } = useParams();
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   });
-  console.log(data, loading);
+
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked,
+      },
+    });
+  };
+
   if (loading) {
     return <h1>Fetching movie...</h1>;
   }
@@ -68,7 +87,9 @@ export default function Movie() {
       <Column>
         <Title>{loading ? "Loading..." : `${data.movie.title}`}</Title>
         <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
-        <button>{data?.movie?.isLiked ? "UnLike" : "Like"}</button>
+        <button onClick={onClick}>
+          {data?.movie?.isLiked ? "UnLike" : "Like"}
+        </button>
       </Column>
       <Image bg={data?.movie?.medium_cover_image} />
     </Container>
